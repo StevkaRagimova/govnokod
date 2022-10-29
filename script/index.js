4306e4de3379a4272d2de2301f45eb2f7afdb718
Julia Zemskova <ms.druggie@gmail.com>
2022-10-29 08:01:17 +0500

    first review

--- script/index.js
+++ script/index.js
@@ -1,41 +1,37 @@
-const profileEditBtn = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const popupBtnClose = document.querySelector('.popup__button-close');
-let popupForm = document.querySelector ('.form');
-const profileName = document.querySelector('.profile__name');
+const profileEditBtn = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const popupBtnClose = document.querySelector(".popup__button-close");
+let popupForm = document.querySelector(".form");
+const profileName = document.querySelector(".profile__name");
 const formInputName = document.querySelector('input[name="form__input_name"]');
-const profileProfession = document.querySelector('.profile__workplace');
+const profileProfession = document.querySelector(".profile__workplace");
 const formInputProfession = document.querySelector('input[name="form__input_workplace"]');
 
-
 function openPopup() {
-  popup.classList.add('popup_opened');
+    popup.classList.add("popup_opened");
 }
 
 function closePopup() {
-  popup.classList.remove('popup_opened');
+    popup.classList.remove("popup_opened");
 }
 
 function addValueInValue() {
-  formInputName.value = profileName.textContent;
-  formInputProfession.value = profileProfession.textContent;
+    formInputName.value = profileName.textContent;
+    formInputProfession.value = profileProfession.textContent;
 }
 
 function formSubmitHandler(form) {
-  form.preventDefault();
-  profileName.textContent = formInputName.value;
-  profileProfession.textContent = formInputProfession.value;
-  closePopup();
+    form.preventDefault();
+    profileName.textContent = formInputName.value;
+    profileProfession.textContent = formInputProfession.value;
+    closePopup();
 }
 
+profileEditBtn.addEventListener("click", openPopup);
+popupBtnClose.addEventListener("click", closePopup);
+profileEditBtn.addEventListener("click", () => {
+    openPopup();
+    addValueInValue();
+});
 
-profileEditBtn.addEventListener('click', openPopup);
-popupBtnClose.addEventListener('click', closePopup);
-profileEditBtn.addEventListener('click', () => {
-  openPopup();
-  addValueInValue();
-})
-
-popupForm.addEventListener('submit', formSubmitHandler); 
-
-
+popupForm.addEventListener("submit", formSubmitHandler);
